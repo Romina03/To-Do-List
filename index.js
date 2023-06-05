4333b99ae899a8d9131b47bcb97a74b843a931bf
Romina03 <86558548+Romina03@users.noreply.github.com>
2023-06-05 03:15:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 function addToList() {
     var newItem = document.createElement("li");
     var task = document.querySelector(".input-bar").value;
@@ -9,18 +7,21 @@ function addToList() {
     newButton.innerHTML = "Remove";
     newButton.className = "remove-button";  
 
+    var check = document.createElement("input");
+    check.setAttribute("type", "checkbox");
+    check.className = "check-box";
+
     newButton.onclick = function() {
-        var e = this.parentElement;
-        e.remove();
+        this.parentElement.remove();
     }
      
-
+    newItem.appendChild(check);
     newItem.appendChild(givenInput);
     newItem.appendChild(newButton);
 
-    if(task != "") {
+    if (task != "") {
         document.querySelector(".task-list").appendChild(newItem);
-     
+        document.querySelector(".input-bar").value = "";
     }
     
 }
@@ -31,9 +32,9 @@ function clearList() {
     for (var i = 0; i < numList; i++) {
         document.getElementsByTagName("li")[0].remove();
     }
+    document.querySelector(".input-bar").value = "";
   
 }
 
 document.querySelector(".enter-button").addEventListener("click", addToList);
 document.querySelector(".reset-button").addEventListener("click", clearList);
-
